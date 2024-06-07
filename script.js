7012fe54df7fdfe33029a2d1f081bf06e2516d4b
shaniakarim <156203203+shaniakarim@users.noreply.github.com>
2024-06-07 18:33:43 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-document.getElementById('my-button').addEventListener('click',function(){
+// Change h1 text and print to console
+document.getElementById('my-button').addEventListener('click', function() {
     console.log('Hello world');
-    document.querySelector('h1').textContent='Moi maailma';
+    document.getElementById('heading').textContent = 'Moi maailma'; // Check ID: heading
 });
+
+// Add list item with custom text from textarea
 document.getElementById('add-data').addEventListener('click', function() {
-    let ul = document.getElementById('my-list');
-    let li = document.createElement('li');
-    let text = document.getElementById('list-text').value;
-    li.textContent = text;
-    ul.appendChild(li);
+    let ul = document.getElementById('my-list');  // Check ID: my-list
+    let li = document.createElement('li');       
+    let text = document.getElementById('list-text').value; // Check ID: list-text
+    li.textContent = text;  
+    ul.appendChild(li);     
 });
